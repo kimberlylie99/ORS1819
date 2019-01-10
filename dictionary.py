1c923daa5719f286f4cca68dfaa0a1c89ea8d7f8
Ruth Pavoor <rgp321@gmail.com>
2019-01-10 14:24:50 -0500

    updated the disctionary class

--- dictionary.py
+++ dictionary.py
@@ -1,14 +1,13 @@
 ## Dictionary file
 
 class BookList:
-    books = {
-        'Name' : '',
-        'Pages' : ''
-        }
-    
-    def __init__ (self,name,pages):
-        self.books['Name'] = name
-        self.books['Pages'] = pages
-        
-    def getBooks(self)
-        return list(self.books.values())
+    books= {}
+    def __init__(self,bookName,pages):
+        self.books[bookName]=pages
+        #self.books[pages]= pages
+        print('Initialed a Booklist with one book!')
+    def getBooks(self):
+        return list(self.books.keys())
+    def addBook(self, bookName, pages):
+        self.books[bookName] = pages
+        print("you've added a new book!")
