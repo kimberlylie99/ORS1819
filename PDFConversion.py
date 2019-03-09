051037e237b57465de4105edfcd3dd14dca1d38c
kimberlylie99 <kimberly.lie99@gmail.com>
2019-03-09 16:01:07 -0500

    edit conversion file

--- PDFConversion.py
+++ PDFConversion.py
@@ -1,4 +1,5 @@
 import PyPDF2
+import fileinput
 
 newfile = open("TheHelpConversion.txt","w")
 file = open("TheHelp.pdf","rb")
@@ -10,9 +11,12 @@ while(index<pdfreader.getNumPages()):
     pageobj = pdfreader.getPage(index)
     #print(pageobj.extractText())
     newfile.write(pageobj.extractText())
-    index= index+1;
+    index= index+1
 file.close()
 
+for line in fileinput.FileInput("file",inplace=1):
+    if line.rstrip():
+        print line
 newfile = open("TheHelpConversion.txt","r")
 message = newfile.read()
 print(message)
