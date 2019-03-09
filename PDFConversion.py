d39529a35d0bf9f2a25ebf51650968e7f73d6aee
Ruth Pavoor <rgp321@gmail.com>
2019-03-09 16:53:38 -0500

    edit conversion file

--- PDFConversion.py
+++ PDFConversion.py
@@ -1,23 +1,25 @@
 import PyPDF2
 import fileinput
 
-newfile = open("TheHelpConversion.txt","w")
-file = open("TheHelp.pdf","rb")
+newfile = open("CareerFairConversion.txt","wb")
+file = open("Career Fair Crash Course.pdf","rb")
 pdfreader = PyPDF2.PdfFileReader(file)
 #print(pdfreader.getNumPages())
 index = 0;
 while(index<pdfreader.getNumPages()):
 
     pageobj = pdfreader.getPage(index)
-    #print(pageobj.extractText())
-    newfile.write(pageobj.extractText())
+    #pageobj.extractText()
+    text = pageobj.extractText()
+    text = text.encode('utf-8')
+    newfile.write(text)
     index= index+1
 file.close()
 
 #for line in fileinput.FileInput("file",inplace=1):
 #    if line.rstrip():
 #        print(line)
-newfile = open("TheHelpConversion.txt","r")
+newfile = open("CareerFairConversion.txt","r")
 message = newfile.read()
 print(message)
 newfile.close()
