2fdd08d48149257f94318b0565fb78aeafe048db
Ruth Pavoor <rgp321@gmail.com>
2019-03-15 14:51:57 -0400

    used TIKA library to extract txt

--- PDFConversion.py
+++ PDFConversion.py
@@ -1,21 +1,39 @@
 import PyPDF2
 import fileinput
+from tika import parser
+
 
 newfile = open("TheHelpConversion.txt","wb")
-file = open("TheHelp.pdf","rb")
-pdfreader = PyPDF2.PdfFileReader(file)
-#print(pdfreader.getNumPages())
-index = 0;
-while(index<pdfreader.getNumPages()):
+#file = open("TheHelp.pdf","r")
 
-    pageobj = pdfreader.getPage(index)
-    #pageobj.extractText()
-    text = pageobj.extractText()
-    text = text.encode('utf-8')
-    if text != "":
-      newfile.write(text)
-    index= index+1
-file.close()
+raw = parser.from_file("TheHelp.pdf");
+test = raw['content']
+print(test);
+test = test.encode('utf-8')
+newfile.write(test);
+#file.close();
+newfile.close();
+
+#newfile = open("TheHelpConversion.txt","wb")
+#file = open("TheHelp.pdf","rb")
+#pdfreader = PyPDF2.PdfFileReader(file)
+#index = 10;
+#while(index<pdfreader.getNumPages()):
+
+#pageobj = pdfreader.getPage(index)
+#text = pageobj.extractText()
+#print(type(text))
+
+#    text = text.encode('utf-8')
+#    if text != "":
+#      newfile.write(text)
+#    index= index+1
+#file.close()
+
+
+
+
+
 
 #for line in fileinput.FileInput("file",inplace=1):
 #    if line.rstrip():
